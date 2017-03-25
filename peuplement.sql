45a4b7e1520c7a8e1865d7f74c5ab7858e137669
Bastien Hoffstetter <bastien.hoffstetter@free.fr>
2017-03-25 13:04:49 +0100

    Changed my mind n2
    lol fucking conflicts :p

--- peuplement.sql
+++ peuplement.sql
@@ -3,11 +3,11 @@ INSERT INTO Team (nom, score) VALUES ('Panda', 100);
 INSERT INTO Team (nom, score) VALUES ('Renard', 100);
 INSERT INTO Team (nom, score) VALUES ('Tigre', 100);
 
-INSERT INTO User (nom, prenom, pseudo, mail, password, creationDate, score, idTeam) VALUES ('Vasseur', 'Valentin', 'vvasseur', 'vvasseur@ennsat.fr', 'admin', current_date, 100, (SELECT idTeam FROM TEAM WHERE nom = 'Hippopotame'));
-INSERT INTO User (nom, prenom, pseudo, mail, password, creationDate, score, idTeam) VALUES ('Hoffstetter', 'Bastien', 'bhoffstetter', 'bhoffstetter@ennsat.fr', 'admin', current_date, 100, (SELECT idTeam FROM TEAM WHERE nom = 'Panda'));
-INSERT INTO User (nom, prenom, pseudo, mail, password, creationDate, score, idTeam) VALUES ('De Azevedo', 'Mickael', 'mdeazeve', 'mdeazeve@enssat.fr', 'admin', current_date, 100, (SELECT idTeam FROM TEAM WHERE nom = 'Renard'));
-INSERT INTO User (nom, prenom, pseudo, mail, password, creationDate, score, idTeam) VALUES ('Aici', 'Lyes', 'laici', 'laici@ennsat.fr', 'admin', current_date, 100, (SELECT idTeam FROM TEAM WHERE nom = 'Tigre'));
-INSERT INTO User (nom, prenom, pseudo, mail, password, creationDate, score, idTeam) VALUES ('Hackathon', 'Clement', 'chackathon', 'chackathon@ennsat.fr', 'admin', current_date, 100, (SELECT idTeam FROM TEAM WHERE nom = 'Tigre'));
+INSERT INTO Users (nom, prenom, pseudo, mail, password, creationDate, score, idTeam) VALUES ('Vasseur', 'Valentin', 'vvasseur', 'vvasseur@ennsat.fr', 'admin', current_date, 100, (SELECT idTeam FROM TEAM WHERE nom = 'Hippopotame'));
+INSERT INTO Users (nom, prenom, pseudo, mail, password, creationDate, score, idTeam) VALUES ('Hoffstetter', 'Bastien', 'bhoffstetter', 'bhoffstetter@ennsat.fr', 'admin', current_date, 100, (SELECT idTeam FROM TEAM WHERE nom = 'Panda'));
+INSERT INTO Users (nom, prenom, pseudo, mail, password, creationDate, score, idTeam) VALUES ('De Azevedo', 'Mickael', 'mdeazeve', 'mdeazeve@enssat.fr', 'admin', current_date, 100, (SELECT idTeam FROM TEAM WHERE nom = 'Renard'));
+INSERT INTO Users (nom, prenom, pseudo, mail, password, creationDate, score, idTeam) VALUES ('Aici', 'Lyes', 'laici', 'laici@ennsat.fr', 'admin', current_date, 100, (SELECT idTeam FROM TEAM WHERE nom = 'Tigre'));
+INSERT INTO Users (nom, prenom, pseudo, mail, password, creationDate, score, idTeam) VALUES ('Hackathon', 'Clement', 'chackathon', 'chackathon@ennsat.fr', 'admin', current_date, 100, (SELECT idTeam FROM TEAM WHERE nom = 'Tigre'));
 
 INSERT INTO MoyenDeTransport (nom, multiplicateur, tauxSauve) VALUES ('voiture', 0, 0);
 INSERT INTO MoyenDeTransport (nom, multiplicateur, tauxSauve) VALUES ('voiture électrique', 1, 97.07);
